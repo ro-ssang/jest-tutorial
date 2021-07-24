5374adf02245443e31045bf4e1e64f2597c2e73c
rossa <fashionfish@naver.com>
2021-07-24 14:54:53 +0900

    #02.1 유용한 Matchers - toEqual, toStrictEqual

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Jest Tutorial
 
 - 소개, 설치 및 간단한 테스트 작성
+- 유용한 Matchers
+  1. toEqual, toStrictEqual
--- src/fn.test.js
+++ src/fn.test.js
@@ -1,13 +1,9 @@
 const fn = require('./fn');
 
-test('1은 1이야.', () => {
-  expect(1).toBe(1);
+test('이름과 나이를 전달받아서 객체를 반환해줘.', () => {
+  expect(fn.makeUser('Mike', 30)).toEqual({ name: 'Mike', age: 30 });
 });
 
-test('2 더하기 3은 5야.', () => {
-  expect(fn.add(2, 3)).toBe(5);
-});
-
-test('3 더하기 3은 5가 아니야.', () => {
-  expect(fn.add(3, 3)).not.toBe(5);
+test('이름과 나이를 전달받아서 객체를 반환해줘.', () => {
+  expect(fn.makeUser('Mike', 30)).toStrictEqual({ name: 'Mike', age: 30 });
 });
